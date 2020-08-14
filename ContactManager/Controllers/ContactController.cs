eab9fec079d70136d7a3e34862659aa339bed747
Nick Whynter <nekitkee@gmail.com>
2020-08-14 17:58:16 +0300

    task done

---
+++ ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactManager.Models;
+using ContactManager.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
+        [HttpGet]
+        [Route("{id?}")]
+        public List<Contact> Get([FromRoute]int? id)
+        {
+            if (id!=null)
+            {
+                return new List<Contact>() { contactRepository.GetById(id.Value) };
+            }
+
+           
+            return contactRepository.GetAllContacts();
+        }
+
+
+        [HttpPost]
+        
+        public IActionResult AddContact([FromBody]Contact contact)
+        {
+           if(contactRepository.SaveContact(contact))
+           {
+                return this.Ok();
+           }
+
+            return this.UnprocessableEntity();
+
+        } 
+       
+
+
+
+
+    }
+}
---
+++ ContactManager/Services/ContactRepository.cs
@@ -0,0 +1,66 @@
+﻿using ContactManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ContactManager.Services
+{
+    public class ContactRepository
+    {
+
+        private const string databaseFileName = "database.json";
+
+
+        public Contact GetById(int id)
+        {
+
+            return this.GetAllContacts().Find(x => x.Id == id);
+        }
+
+
+        public List<Contact> GetAllContacts()
+        {
+
+            string jsonData = System.IO.File.ReadAllText(databaseFileName);
+            return string.IsNullOrEmpty(jsonData) ? new List<Contact>() : 
+                JsonSerializer.Deserialize<List<Contact>>(jsonData);
+
+        }
+
+        public bool SaveContact(Contact contact)
+        {
+            try
+            {
+                List<Contact> contacts = GetAllContacts();
+                contacts.Add(contact);
+                System.IO.File.WriteAllText(databaseFileName, JsonSerializer.Serialize(contacts));
+                return true;
+            }
+            catch 
+            {
+                return false;
+            }
+        }
+
+
+
+        public List<Contact> GetAllContactsOld()
+        {
+            return new List<Contact>
+            {
+                new Contact
+                {
+                    Id=1,
+                    Name="Glen Block"
+                } ,
+                new Contact{
+                    Id=2,
+                    Name = "Dan Roth"
+                }
+            };
+        }
+
+    }
+}
